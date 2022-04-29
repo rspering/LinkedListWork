89a8a0db6fd46e91bf4d5cdeba2de4a2de86ac7c
Spering <rjstrd@rc22xcs213.managed.mst.edu>
2022-04-29 13:34:20 -0500

    test me pls

--- MyList.hpp
+++ MyList.hpp
@@ -1,3 +1,4 @@
+using namespace std;
 
 void get_identity(std::string & my_id)
 {
@@ -60,7 +61,11 @@ MyList<T> & MyList<T>::operator=(const MyList<T> &source)
 template <typename T>
 void MyList<T>::assign(int count, const T &value)
 {
-
+  clear();
+  for(int i = 0; i < count; i++)
+  {
+    push_back(value);
+  }
 }
 
 
@@ -80,26 +85,19 @@ void MyList<T>::clear()
 template <typename T>
 T & MyList<T>::front()
 {
-
+  return m_sentinel->m_next->m_element;
 }
 
 
 template <typename T>
 T & MyList<T>::back()
 {
-
+  return m_sentinel->m_prev->m_element;
 }
 
 
 template <typename T>
 void MyList<T>::push_front(const T &x)
-{
-
-}
-
-
-template <typename T>
-void MyList<T>::push_back(const T &x)
 {
   if(size() == 0)
   {
@@ -109,17 +107,48 @@ void MyList<T>::push_back(const T &x)
   }
   else
   {
-    Node<T> *newNode = new Node<T>(x, nullptr, nullptr);
-    m_sentinel->m_prev->m_next = newNode;
+    Node<T> *newNode = new Node<T>(x, nullptr, m_sentinel->m_next);
+    m_sentinel->m_next->m_prev = newNode;
     m_sentinel->m_next = newNode;
   }
+  m_size++;
+}
+
+
+template <typename T>
+void MyList<T>::push_back(const T &x)
+{
+  if(m_size == 0)
+  {
+    Node<T> *newNode = new Node<T>(x, nullptr, nullptr);
+    m_sentinel->m_next = newNode;
+    m_sentinel->m_prev = newNode;
+  }
+  else
+  {
+    Node<T> *newNode = new Node<T>(x, m_sentinel->m_prev, nullptr);
+    m_sentinel->m_prev->m_next = newNode;
+    m_sentinel->m_prev = newNode;
+  }
+  m_size++;
 }
 
 
 template <typename T>
 void MyList<T>::pop_front()
 {
-
+	if(m_size  <= 1)
+  {
+    clear();
+  }
+  else
+  {
+    Node<T> *temp = m_sentinel->m_next;
+    m_sentinel->m_next = m_sentinel->m_next->m_next;
+    m_sentinel->m_next->m_prev = nullptr;
+    m_size--;
+    delete temp;
+  }
 }
 
 
@@ -127,35 +156,105 @@ void MyList<T>::pop_front()
 template <typename T>
 void MyList<T>::pop_back()
 {
-
+  if(m_size  <= 1)
+	{
+		clear();
+	}
+  else
+  {
+    Node<T> *temp = m_sentinel->m_prev;
+    m_sentinel->m_prev = m_sentinel->m_prev->m_prev;
+    m_sentinel->m_prev->m_next = nullptr;
+    m_size--;
+    delete temp;
+  }	
 }
 
 
 template <typename T>
 void MyList<T>::insert(int i, const T &x)
 {
+  Node<T> *curr = nullptr; 
+	if(i >= size())
+	{
+		push_back(x);
+	}
+  else if(i <= 0)
+	{
+		push_front(x);
+	}
+  else
+	{
+		curr = m_sentinel;
+    for(int j = 0; j < i; j++)
+    {
+      curr = curr->m_next;
+    }
 
+    Node<T> *newNode = new Node<T> (x, curr, curr->m_next);
+    curr->m_next->m_prev = newNode;
+    curr->m_next = newNode;
+    m_size++;
+	}
 }
 
 
 template <typename T>
 void MyList<T>::remove(T value)
-{
-
+{  
+  Node<T> *curr = m_sentinel;
+	for(int i = 0; i < m_size && curr != nullptr; i++)
+	{
+		curr = curr -> m_next;
+		if( curr ->  m_element == value)
+		{
+			erase(i);
+			i--;
+		}
+	}
 }
 
 
 template <typename T>
 void MyList<T>::erase(int i)
 {
-
+  Node<T> *currentptr = m_sentinel;
+	if(i == 0)
+	{
+		pop_front();
+	}
+  else if(i == m_size-1)
+	{
+		pop_back();
+	}
+  else
+	{
+	  Node<T> *currentptr = m_sentinel->m_next;
+		for(int j = 0; j < i && j < m_size-1; j++)
+		{
+			currentptr = currentptr->m_next;
+		}
+		
+		currentptr->m_prev->m_next = currentptr->m_next;
+		currentptr->m_next->m_prev = currentptr->m_prev;
+		delete currentptr;
+		m_size--;
+	}
 }
 
 
 template <typename T>
 void MyList<T>::reverse()
 {
-
+  Node<T> *currentptr = m_sentinel->m_next;
+	while(currentptr != nullptr)
+	{
+		std::swap(currentptr->m_prev, currentptr->m_next);
+		currentptr = currentptr->m_prev;	
+	}
+	std::swap(m_sentinel->m_prev, m_sentinel->m_next);
+	currentptr = nullptr;
+	delete currentptr;
 }
 
 template <typename T>
